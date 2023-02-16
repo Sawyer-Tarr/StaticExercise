6f3191273f171ae9543919dd41b5a0e98cc71702
sawyer tarr <sawyertarr@outlook.com>
2023-02-16 16:32:13 -0600

    Static

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using StaticExercise;
+Console.WriteLine("Please enter the temperature in Fahrenheit?");
+
+double temp = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"The temperatue in Celcius is {TempConverter.FahrenheitToCelsius(temp)}.");
+
+Console.WriteLine("What is the temperature outside in Celcius?");
+
+double temp1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"The temperature in Fahrenheit is {TempConverter.CelsiusToFahrenheit(temp1)}.");
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double temp1) 
+        {
+            return (temp1 - 32) * .5556;
+        }
+        public static double CelsiusToFahrenheit(double temp2) 
+        {
+            return (temp2 * 1.8) + 32;
+        }
+
+
+    }
+}
